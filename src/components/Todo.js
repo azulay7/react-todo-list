52b953d242c6f1485aea7c44ae891137df526169
Amit Azulay <Amit.Azulay@orbotech.com>
2022-02-27 13:29:36 +0200

    work

---
+++ src/components/Todo.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { RiCloseCircleLine } from 'react-icons/ri'
+import { RiEdit2Line } from 'react-icons/ri'
+import TodoForm from './TodoForm'
+
+
+function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+    const [edit, setEdit] = useState(
+        {
+            id: null,
+            value: ''
+        })
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    }
+
+    if(edit.id){
+        return <TodoForm edit={edit} onSubmit={submitUpdate}></TodoForm>
+    }
+
+    return todos.map((todo, index) => (
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            key={index}>
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}</div>
+            <div className='icons'>
+                <RiCloseCircleLine
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'>
+                </RiCloseCircleLine>
+                <RiEdit2Line onClick={() => setEdit(todo)}
+                    className='edit-icon'></RiEdit2Line> </div>
+        </div>))
+}
+
+export default Todo
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-export default function TodoForm() {
+export default function TodoForm(props) {
 
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState(props.edit? props.edit.text: '')
+
+
+    const inputRef = useRef(null)
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        });
+        setInput('');
+    }
     return (
-        <form className='todo-form'>
-            <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input'></input>
-            <button className='todo-button'>Add Todo</button>
+        <form className='todo-form' onSubmit={handleSubmit}>
+            {props.edit? (<><input type='text' placeholder='Edit todo'
+                value={input} name='text' className='todo-input edit'
+                onChange={handleChange}
+                ref={inputRef}></input>
+            <button className='todo-button'>Update</button></>) :
+            (<><input type='text' placeholder='Add todo'
+                value={input} name='text' className='todo-input'
+                onChange={handleChange}
+                ref={inputRef}></input>
+            <button className='todo-button'>Add Todo</button></>)}
+            
         </form>
     )
 }
